5499bde793450656891ccd728db3d696c343786e
AJ-Simon <uskwan@gmail.com>
2020-04-24 23:26:41 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Vanilla_JS_exercise
+ 
---
+++ index.js
@@ -0,0 +1,22 @@
+/* const monday = "Mon";
+const tue = "Tue";
+const wed = "Wed";
+const thu = "Thu";
+const fri = "Fri";
+
+console.log(monday, tue, wed, thu, fri); */
+
+/* const daysOfWeek = ["Mon", "Thu", "Wed", "Thu", "Fri"];
+
+console.log(daysOfWeek[2]); */
+
+const SimonInfo = {
+    name:"Uisik_Kwon",
+    age:33,
+    gender:"Male",
+    isHandsome:true,
+    favMovies: ["Tomorrow", "Avengers"],
+    favFood: [{name: "Pizza", fatty: false}, {name:"pancake", fatty:false}]
+}
+
+console.log(SimonInfo);
